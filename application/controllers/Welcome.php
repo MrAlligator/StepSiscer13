f25c6e61753eba85fd6a2d1f3749bd59eef9f918
MrAlligator <55204773+MrAlligator@users.noreply.github.com>
2021-06-09 20:25:19 +0700

    tampil awal

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +21,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('_partials/head');
+		$this->load->view('_partials/sidebar');
+		$this->load->view('_partials/topbar');
+		$this->load->view('blank');
+		$this->load->view('_partials/foot');
 	}
 }
